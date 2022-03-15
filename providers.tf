f31c76bffe97ddad38060ad63e21adde6e1a0a31
Bharath <56950806+bablubharath@users.noreply.github.com>
2022-03-15 08:41:38 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,16 @@
-provider "datadog" {
-  version = "~> 2.5"
-  api_key = var.api_key
-  app_key = var.app_key
+terraform {
+  required_providers {
+    datadog = {
+      source = "DataDog/datadog"
+      version = ">=3.8"
+    }
+  }
 }
 
-provider "external" {
-  version = "~> 1.2"
+
+# Configure the Datadog provider
+provider "datadog" {
+  api_key = "7ddf8533878662f3ac03304fd5c90702"
+  app_key = "080b8a922e16c9823203b29106f41c42ca47caa3"
+  validate = false
 }
